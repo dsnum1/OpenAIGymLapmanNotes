aee5e3ab8e2f1d62c08e790e18fa22a9eeb35e6a
dsnum1 <dsnum1@gmail.com>
2023-08-25 16:35:02 +0800

    understanding env.step() function

--- environment_exploration.py
+++ environment_exploration.py
@@ -6,11 +6,18 @@ env.reset()
 
 print("action_space", env.action_space)
 print("observation_space", env.observation_space)
-print(env.action_space.contains(-1))
-print(env.action_space.sample())
-print(env.observation_space['image'].low)
-print(env.observation_space['image'].high)
-print(env.observation_space['image'].shape)
-print(env.observation_space['image'].dtype)
-print(env.observation_space['image'].sample().shape)
+# print(env.action_space.contains(-1))
+# print(env.action_space.sample())
+# print(env.observation_space['image'].low)
+# print(env.observation_space['image'].high)
+# print(env.observation_space['image'].shape)
+# print(env.observation_space['image'].dtype)
+# print(env.observation_space['image'].sample().shape)
 env.render()
+
+# Executing an action
+# print(env.step(env.action_space.sample()))
+observation, reward, done, _, _1 = env.step(env.action_space.sample())
+
+print(_1)
+
